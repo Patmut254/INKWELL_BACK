4dd7b3a18c36d6c2759f19576bb8de4e851c5333
Patmut254 <anitajehu13@gmail.com>
2026-04-17 21:12:54 +0300

    fix settings STATIC_ROOT

--- blogbackend/settings.py
+++ blogbackend/settings.py
@@ -47,6 +47,7 @@ TIME_ZONE = 'Africa/Nairobi'
 USE_I18N = True
 USE_TZ = True
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
